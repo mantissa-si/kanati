25a9ef4ab7e4e34d07a2c257b4d457b60d38e2cb
Tim Rekelj <62036887+timrekelj@users.noreply.github.com>
2024-01-30 10:50:04 +0100

    Update +page.svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -8,6 +8,6 @@
 	<meta name="title" content="Kanati" />
 	<meta name="description" content="Svetujemo in investiramo v nepremičnine, premičnine, v podjetja z lastnimi izdelki ali podjetja, ki potrebujejo hitro likvidnost za ustvarjanje večje dodane vrednosti."/>
 	<meta name="keywords" content="Svetovanje, finance, investicije, investiranje, kanati, nepremičnine, premičnine"/>
-	<meta name="author" content="Iztok Rekelj, Žiga Kosmatin, Vesna Stipetić"/>
+	<meta name="author" content="Iztok Rekelj, Žiga Kosmatin, Nika Lebar"/>
 	<meta name="viewport" content="width=device-width, initial-scale=1.0"/>
 </svelte:head>
